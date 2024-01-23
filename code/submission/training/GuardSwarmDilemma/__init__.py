eee7cfe23eaf75ad720b10fbb8aa5ec4984e2d4b
RepentForYourCyns <32701796+RepentForYourCyns@users.noreply.github.com>
2024-01-23 17:11:11 +0000

    Fixed GuardSwarmDilemma package init

---
+++ code/submission/training/GuardSwarmDilemma/__init__.py
@@ -0,0 +1,4 @@
+from GuardSwarmDilemma.Agent import Agent
+from GuardSwarmDilemma.Simulation import Simulation
+from GuardSwarmDilemma.Strategy import AbstractStrategy, DQNStrategy
+from GuardSwarmDilemma.Template import TemplateAgent
